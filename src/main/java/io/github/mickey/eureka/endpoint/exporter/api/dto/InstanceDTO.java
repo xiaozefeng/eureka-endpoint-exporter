ea3f232479dad5ea206c49d096319d0c03750f83
mickey <qq523107430@163.com>
2021-04-23 11:56:21 +0800

    增加可导出最大数量

--- src/main/java/io/github/mickey/eureka/endpoint/exporter/api/dto/InstanceDTO.java
+++ src/main/java/io/github/mickey/eureka/endpoint/exporter/api/dto/InstanceDTO.java
@@ -17,47 +17,47 @@ public class InstanceDTO {
     private String ipAddr;
     @SerializedName("status")
     private String status;
-    @SerializedName("overriddenStatus")
-    private String overriddenStatus;
-    @SerializedName("countryId")
-    private Integer countryId;
-    @SerializedName("leaseInfo")
-    private LeaseInfoDTO leaseInfo;
-    @SerializedName("homePageUrl")
-    private String homePageUrl;
-    @SerializedName("statusPageUrl")
-    private String statusPageUrl;
-    @SerializedName("healthCheckUrl")
-    private String healthCheckUrl;
-    @SerializedName("vipAddress")
-    private String vipAddress;
-    @SerializedName("secureVipAddress")
-    private String secureVipAddress;
-    @SerializedName("isCoordinatingDiscoveryServer")
-    private String isCoordinatingDiscoveryServer;
-    @SerializedName("lastUpdatedTimestamp")
-    private String lastUpdatedTimestamp;
-    @SerializedName("lastDirtyTimestamp")
-    private String lastDirtyTimestamp;
-    @SerializedName("actionType")
-    private String actionType;
+    // @SerializedName("overriddenStatus")
+    // private String overriddenStatus;
+    // @SerializedName("countryId")
+    // private Integer countryId;
+    // @SerializedName("leaseInfo")
+    // private LeaseInfoDTO leaseInfo;
+    // @SerializedName("homePageUrl")
+    // private String homePageUrl;
+    // @SerializedName("statusPageUrl")
+    // private String statusPageUrl;
+    // @SerializedName("healthCheckUrl")
+    // private String healthCheckUrl;
+    // @SerializedName("vipAddress")
+    // private String vipAddress;
+    // @SerializedName("secureVipAddress")
+    // private String secureVipAddress;
+    // @SerializedName("isCoordinatingDiscoveryServer")
+    // private String isCoordinatingDiscoveryServer;
+    // @SerializedName("lastUpdatedTimestamp")
+    // private String lastUpdatedTimestamp;
+    // @SerializedName("lastDirtyTimestamp")
+    // private String lastDirtyTimestamp;
+    // @SerializedName("actionType")
+    // private String actionType;
 
 
-    @NoArgsConstructor
-    @Data
-    public static class LeaseInfoDTO {
-        @SerializedName("renewalIntervalInSecs")
-        private Integer renewalIntervalInSecs;
-        @SerializedName("durationInSecs")
-        private Integer durationInSecs;
-        @SerializedName("registrationTimestamp")
-        private Long registrationTimestamp;
-        @SerializedName("lastRenewalTimestamp")
-        private Long lastRenewalTimestamp;
-        @SerializedName("evictionTimestamp")
-        private Integer evictionTimestamp;
-        @SerializedName("serviceUpTimestamp")
-        private Long serviceUpTimestamp;
-    }
+    // @NoArgsConstructor
+    // @Data
+    // public static class LeaseInfoDTO {
+    //     @SerializedName("renewalIntervalInSecs")
+    //     private Integer renewalIntervalInSecs;
+    //     @SerializedName("durationInSecs")
+    //     private Integer durationInSecs;
+    //     @SerializedName("registrationTimestamp")
+    //     private Long registrationTimestamp;
+    //     @SerializedName("lastRenewalTimestamp")
+    //     private Long lastRenewalTimestamp;
+    //     @SerializedName("evictionTimestamp")
+    //     private Integer evictionTimestamp;
+    //     @SerializedName("serviceUpTimestamp")
+    //     private Long serviceUpTimestamp;
+    // }
 
 }
--- src/main/java/io/github/mickey/eureka/endpoint/exporter/controller/IndexController.java
+++ src/main/java/io/github/mickey/eureka/endpoint/exporter/controller/IndexController.java
@@ -1,21 +1,18 @@
 package io.github.mickey.eureka.endpoint.exporter.controller;
 
-import com.google.common.cache.Cache;
 import io.github.mickey.eureka.endpoint.exporter.api.EurekaAPI;
+import io.github.mickey.eureka.endpoint.exporter.api.dto.AppListResp;
+import io.github.mickey.eureka.endpoint.exporter.api.dto.ApplicationDTO;
 import io.github.mickey.eureka.endpoint.exporter.api.dto.InstanceDTO;
 import io.github.mickey.eureka.endpoint.exporter.vo.EurekaServiceVO;
 import lombok.SneakyThrows;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 @RestController
@@ -30,39 +27,47 @@ public class IndexController {
     private final EurekaAPI eurekaAPI;
 
 
-    @GetMapping("apps")
-    public Object getApps() throws IOException {
-        return eurekaAPI.getApps().execute().body();
-    }
+    // @GetMapping("apps")
+    // public Object getApps() throws IOException {
+    //     return eurekaAPI.getApps().execute().body();
+    // }
 
-    @Autowired
-    private Cache<String, List<InstanceDTO>> cache;
+    // @Autowired
+    // private Cache<String, List<InstanceDTO>> cache;
 
     @SneakyThrows
     @GetMapping("search/{appid}")
     public Object getApp(@PathVariable String appid) {
-        // cache for all name
-        List<String> names = new ArrayList<>();
-        for (Map.Entry<String, List<InstanceDTO>> entry : cache.asMap().entrySet()) {
-            names.add(entry.getKey());
-        }
+        // // cache for all name
+        // List<String> names = new ArrayList<>();
+        // for (Map.Entry<String, List<InstanceDTO>> entry : cache.asMap().entrySet()) {
+        //     names.add(entry.getKey());
+        // }
+        //
+        // List<String> searchResult = new ArrayList<>();
+        // // fuzzy search appid
+        // for (String name : names) {
+        //     if (name.toLowerCase().contains(appid)) {
+        //         searchResult.add(name);
+        //     }
+        // }
+        //
+        // List<EurekaServiceVO> result = new ArrayList<>();
+        // // return all find result
+        // for (String possibleAppId : searchResult) {
+        //     final List<InstanceDTO> instances = cache.get(possibleAppId, ()-> Objects.requireNonNull(eurekaAPI.getApp(possibleAppId).execute().body()).getApplication().getInstance());
+        //     EurekaServiceVO vo = convertToVO(possibleAppId, instances);
+        //     result.add(vo);
+        // }
+        // return result;
 
-        List<String> searchResult = new ArrayList<>();
-        // fuzzy search appid
-        for (String name : names) {
-            if (name.toLowerCase().contains(appid)) {
-                searchResult.add(name);
-            }
-        }
+        final AppListResp resp = eurekaAPI.getApps().execute().body();
+        Assert.notNull(resp, "获取所有apps失败");
 
-        List<EurekaServiceVO> result = new ArrayList<>();
-        // return all find result
-        for (String possibleAppId : searchResult) {
-            final List<InstanceDTO> instances = cache.get(possibleAppId, ()-> Objects.requireNonNull(eurekaAPI.getApp(possibleAppId).execute().body()).getApplication().getInstance());
-            EurekaServiceVO vo = convertToVO(possibleAppId, instances);
-            result.add(vo);
-        }
-        return result;
+        final List<ApplicationDTO> apps = resp.getApplications().getApplication();
+        final List<ApplicationDTO> filteredName = apps.stream().filter(e -> e.getName().toLowerCase().contains(appid))
+                .collect(Collectors.toList());
+        return filteredName.stream().map(e -> convertToVO(e.getName(), e.getInstance()));
     }
 
     private EurekaServiceVO convertToVO(String appName, List<InstanceDTO> instances) {
